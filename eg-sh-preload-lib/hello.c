fbb84efd08a930d31695d3d311482cefda3e80b1
viggy28 <villan.lampard.epl@gmail.com>
2024-03-25 11:44:51 -0700

    update

--- eg-sh-preload-lib/hello.c
+++ eg-sh-preload-lib/hello.c
@@ -10,6 +10,9 @@ PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(hello);
 
+// Anything to do when the library is loaded
+void _PG_init(void) { elog(INFO, "hello from _PG_init"); }
+
 Datum hello(PG_FUNCTION_ARGS) {
   PG_RETURN_TEXT_P(cstring_to_text("Hello, world from shared library!!"));
 }
